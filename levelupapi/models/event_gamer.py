2d05d5370633a10644d0cc2b27cc00cd65cecf8c
Melissa Carter <mkd.princess@gmail.com>
2024-11-21 20:30:44 -0600

    additions

--- levelupapi/models/event_gamer.py
+++ levelupapi/models/event_gamer.py
@@ -1,7 +1,6 @@
+from .gamer import Gamer
 from django.db import models
 from .event import Event
-from .gamer import Gamer
-
 class EventGamer(models.Model):
     gamer = models.ForeignKey(Gamer, on_delete=models.CASCADE)  # Foreign key to Gamer
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
