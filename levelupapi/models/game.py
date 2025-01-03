df6e6967b6baa8bda738f3dd72a1128317bd4ac1
Melissa Carter <mkd.princess@gmail.com>
2025-01-03 07:31:52 -0600

    testing

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -3,6 +3,7 @@ from .game_type import GameType
 from django.db import models
 
 class Game(models.Model):
+    game_type = models.ForeignKey(GameType, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=50)
     maker = models.CharField(max_length=50)         
     gamer = models.ForeignKey(Gamer, on_delete=models.CASCADE) 
--- levelupapi/views/__init__.py
+++ levelupapi/views/__init__.py
@@ -1,1 +1,5 @@
 from .game_type import GameTypeView
+from. game import GameView
+from .gamer import GamerView
+from .event import EventView
+from .auth import check_user, register_user
